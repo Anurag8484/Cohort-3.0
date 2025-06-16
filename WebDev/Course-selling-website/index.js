d7d89fb91d6c8fa26ce3fbcb857e6256fc3fe0a4
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-06-16 14:08:15 +0530

    Add Skeleton routes

---
+++ WebDev/Course-selling-website/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { z } = require('zod');
+const bcrypt = require('bcrypt');
+
+const app = express();
+app.use(express.json())
+ 
+
+
+app.post("/user/signup", async (req,res)=>{
+    
+})
+app.post("/user/signin", async (req,res)=>{
+
+})
+app.get("/user/purchases", async (req,res)=>{
+
+})
+app.post("/course/purchase", async (req,res)=>{
+
+})
+app.post("/user/signup", async (req,res)=>{
+
+})
+app.post("/courses/", async (req,res)=>{
+
+})
