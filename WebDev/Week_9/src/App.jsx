1014f2a82105392634694e9338b95185099e6188
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-06-21 16:23:54 +0530

    addPost logic

--- WebDev/Week_9/src/App.jsx
+++ WebDev/Week_9/src/App.jsx
@@ -1,29 +1,63 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Post } from "./Post";
 
-function App(){
-return(
-  <div style={{ background:"dfe6e9", height: "100vh"}}>
-    <ToggleMessage />
-    <ToggleMessage />
-    <ToggleMessage />
-  </div>
-)
-}
+function App() {
 
-const ToggleMessage = ()=>{
-  const[notificationCount, setNotificationCount] = useState(0);   // Defining a State Variable
-//  WHen the val of state var changes, => The component that uses that state var; re-renders!!/
-  console.log('re-render')
-  function increment(){
-    setNotificationCount(notificationCount +1 )
+  const [posts,setPosts] = useState([]);
+
+  // const posts = [
+  //   {
+  //     name: "harkirat",
+  //     subtitle: "10000 followers",
+  //     time: "2m ago",
+  //     image:
+  //       "https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg",
+  //     description: "Haha Danger 2.0",
+  //   },
+  // ];
+
+
+
+  const postComponents = posts.map(post => <Post
+    name = {post.name}
+    subtitle= {post.subtitle}
+    time = {post.time}
+    image = {post.image}
+    description = {post.description}
+    />)
+
+  function addPost() {
+    setPosts([...posts,{
+      name: "Harkirat2",
+      subtitle: "50012300 +followers",
+      time: "1444m ago",
+      image:
+        "https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg",
+      description: "Cohort 3.0",
+    }])
+
   }
+
   return (
-    <div>
-      <button onClick={increment}>Increase Count</button> {notificationCount}
+    <div
+      style={{
+        background: "#dfe6e9",
+        height: "100%",
+      }}
+    >
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <div>
+          <button type="button" onClick={addPost}>
+            Add Post
+          </button>
+          <div>
+            {postComponents}
+            <br />
+          </div>
+        </div>
+      </div>
     </div>
   );
-
-
 }
 
 export default App;
