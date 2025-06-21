9ae1d3e2e0eb0ff2d655fa8ed78758c0127429fe
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-06-21 11:07:04 +0530

    Start dependency array

--- WebDev/Week_9/src/App.jsx
+++ WebDev/Week_9/src/App.jsx
@@ -1,57 +1,7 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [visible, setVisibility] = useState(true);
-
-  useEffect(function(){
-    setInterval(function(){
-      setVisibility(c => !c)
-    },5000)
-  },[])
-
-  return (
-    <div>
-      {visible && <Counter></Counter>}
-
-    </div>
-  )
-
-}
-// New Component
-function Counter(){
-
-  const [count, setCount] = useState(0);
-  function increaseCount(){
-    setCount(count+1);
-  }
   
-  function decreaseCount(){
-    if (count >0){
-      setCount(count-1);
-    }else{
-      alert("Negatives are not allowed")
-    }
-  }
-  function resetCount(){
-    setCount(0);
-  }
-
-  useEffect(function(){
-    setInterval(function(){
-      setCount(count => count+1);
-    },1000)
-  },[]);
-
-
-  return (
-    <div>
-      <h1>{count}</h1>
-      <button onClick={increaseCount} > Increase Count</button>
-      <button onClick={decreaseCount}  > Decrease Count</button>
-      <br />
-      <button onClick={resetCount}  > Reset Count</button>
-    </div>
-  )
 }
 
 export default App
