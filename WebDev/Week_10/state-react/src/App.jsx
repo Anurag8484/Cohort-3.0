2af8d093ac40056a2609f007b72761ea5ba82ac0
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-06-24 14:48:18 +0530

    Basic bulb ~ State Exp

--- WebDev/Week_10/state-react/src/App.jsx
+++ WebDev/Week_10/state-react/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <LightBulb />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+function LightBulb() {
+  const [bulbOn, setBulbOn] = useState(true);
+  return (
+    <div>
+      <BulbState bulbOn={bulbOn} />
+      <ToggleBulbState setBulbOn={setBulbOn} />
+    </div>
+  );
+}
+
+function BulbState({bulbOn}) {
+  return <div>{bulbOn ? "Bulb On" : "Bulb Off"}</div>;
+}
+
+function ToggleBulbState({setBulbOn}) {
+  return (
+    <div>
+      <button onClick={()=>{setBulbOn(c=>!c)}}>Toggle the button </button>
+    </div>
+  );
+}
+
+export default App;
