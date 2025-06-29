55527e018426a47672d2531c9bc6850563093f46
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-06-29 15:26:48 +0530

    start wishlist logic

--- WebDev/assignments-main/week-11/amazonCart/src/components/Navbar.jsx
+++ WebDev/assignments-main/week-11/amazonCart/src/components/Navbar.jsx
@@ -1,6 +1,22 @@
-export function Navbar(){
-    return(
-        <>
-        </>
-    )
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
+import "../Navbar.module.css"
+export function Navbar() {
+    const navigate = useNavigate();
+    
+  return (
+    <>
+      <header>
+        <div className="nav-left" onClick={()=>navigate('/')}>
+            Amazon
+        </div>
+        <div className="nav-right">
+            <i>
+          <FontAwesomeIcon onClick={()=>navigate('/cart')} icon={faCartShopping} />
+            </i>
+        </div>
+      </header>
+    </>
+  );
 }
--- WebDev/assignments-main/week-11/amazonCart/src/components/WishList.jsx
+++ WebDev/assignments-main/week-11/amazonCart/src/components/WishList.jsx
@@ -1,10 +1,7 @@
-import { useNavigate } from "react-router-dom";
 
 export function WishList(){
-    const navigate = useNavigate();
     return (
       <>
-        <button onClick={()=>navigate("/cart")}>Cart</button>
       </>
     );
 }
