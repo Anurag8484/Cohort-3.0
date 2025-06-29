59c944744d6741bd8daa553d428eb9800e5c8e45
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-06-29 13:27:03 +0530

    Fetch users and show

--- WebDev/assignments-main/week-10/userApi/src/components/RandomUser.jsx
+++ WebDev/assignments-main/week-10/userApi/src/components/RandomUser.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "../randomUser.css"
+import axios from 'axios'
 
 const RandomUser = () => {
+  const  [user,setUser] = useState([])
+  const [pageCount, setPageCount] = useState(0)
+
+  async function loadUsers(){
+    const res = await axios.get(`https://randomuser.me/api?page=${pageCount}`);
+    
+  }
+
+
+  useEffect(()=>{
+
+    const res = axios
+
+
+  },[pageCount])
+
+
+
+
   return (
     <div>RandomUser</div>
   )
