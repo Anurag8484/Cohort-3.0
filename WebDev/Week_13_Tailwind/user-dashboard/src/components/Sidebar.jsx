87217ccc1a9e00555c15a7bbd34b909a59c9fa4a
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-07-02 09:22:01 +0530

    start user-dash ui

---
+++ WebDev/Week_13_Tailwind/user-dashboard/src/components/Sidebar.jsx
@@ -0,0 +1,21 @@
+export function Sidebar(){
+    return(
+        <>
+        <div className="border-2 border-black h-screen rounded-tr-3xl rounded-br-3xl  absolute left-0 w-20 sticky overflow-hidden top-0 left-0 right-0 bottom-0 duration-700 hover:w-60">
+
+            <div className=" mr-2 mt-4 ml-3">
+                <div className="flex justify-between gap-10   ">
+                    <p className="bg-gray-500 p-1 rounded-lg" >Img</p>
+                    <p className="bg-blue-600 rounded-lg p-1 text-white">Webinar.gg</p>
+                </div>
+
+                <div>
+                    
+                </div>
+            </div>
+        
+        
+        </div>
+        </>
+    )
+}
