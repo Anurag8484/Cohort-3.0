7d69b1b754510c505d052e4ce42f57133c36a795
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-07-02 12:01:45 +0530

    side bar done

--- WebDev/Week_13_Tailwind/user-dashboard/src/components/Sidebar.jsx
+++ WebDev/Week_13_Tailwind/user-dashboard/src/components/Sidebar.jsx
@@ -3,24 +3,28 @@ import { faHouse ,faUsersRectangle ,faCreditCard,faUserAlt, faGear } from "@fort
 export function Sidebar() {
   return (
     <>
-      <div className="border-2 border-black h-screen   absolute left-0 w-15 sticky overflow-hidden top-0 left-0 right-0 bottom-0 duration-700 hover:w-50">
+      <div className="border-2  border-zinc-200 h-screen text-zinc-500  overflow-hidden   w-15  sm:w-60   duration-700 ">
         <div className=" mr-2 mt-4 ml-3">
-          <div className="flex justify-between gap-3   ">
-            <p className="bg-gray-500 p-1 rounded-lg">Img</p>
-            <p className="bg-blue-600 rounded-lg p-1 text-white">Webinar.gg</p>
+          <div className="flex justify-between gap-10   ">
+            <img
+
+              className=" p-1 rounded-lg w-10"
+              src="https://imgs.search.brave.com/ZxTWTxrjaHgy9SwD8k0vNGseY5qHcXwAYlAxC8B-41w/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cmVhbGZha2VwaG90/b3MuY29tL19uZXh0/L2ltYWdlP3VybD0v/aW1hZ2VzL3Byb2Zl/c3Npb25hbC1oZWFk/c2hvdC1pbnB1dC1v/dXRwdXRzL21heC1p/bnB1dC5qcGcmdz0z/ODQwJnE9NzU"
+              alt=""
+            />
           </div>
 
-          <div className="grid mt-10 grid-cols-2 gap-20 gap-x-9  m-2 mb-10 ml-2">
+          <div className="grid mt-10 grid-cols-2 gap-10 mr-5  items-center  align-middle  m-2 mb-10 ml-2 border-zinc-400">
             <FontAwesomeIcon icon={faHouse} />
-            <p>Home</p>
+            <p className="ml-4">Home</p>
             <FontAwesomeIcon icon={faUsersRectangle} />
-            <p>Webinars</p>
+            <p className="ml-4">Webinars</p>
             <FontAwesomeIcon icon={faCreditCard} />
-            <p>Billing</p>
+            <p className="ml-4">Billing</p>
             <FontAwesomeIcon icon={faUserAlt} />
-            <p>User Profile</p>
+            <p className="ml-4">User Profile</p>
             <FontAwesomeIcon icon={faGear} />
-            <p>Setting</p>
+            <p className="ml-4">Setting</p>
           </div>
         </div>
       </div>
