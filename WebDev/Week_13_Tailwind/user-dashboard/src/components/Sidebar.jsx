83c6c8d9118a02064ab31876edc8c6218b44a0d3
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-07-03 12:23:33 +0530

    sidebar logic

--- WebDev/Week_13_Tailwind/user-dashboard/src/components/Sidebar.jsx
+++ WebDev/Week_13_Tailwind/user-dashboard/src/components/Sidebar.jsx
@@ -2,12 +2,12 @@ export function Sidebar() {
   return (
     <div className="flex">
 
-  <div className="bg-pink-200 sm:w-60 h-screen w-20 duration-550">
+  <div className=" md:w-60 h-screen w-0 transition-all duration-500 text-center">
       SideBar
 
     </div>
 
-    <div className="bg-green-500 w-full">
+    <div className="bg-green-500 w-full text-center">
       Content
     </div>
     </div>
