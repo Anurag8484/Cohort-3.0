d7c9f5d0f4533c5c5efa437907c4cf3d57d872b6
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-07-03 11:52:10 +0530

    apply responsive logic

--- WebDev/Week_13_Tailwind/user-dashboard/src/components/Sidebar.jsx
+++ WebDev/Week_13_Tailwind/user-dashboard/src/components/Sidebar.jsx
@@ -1,7 +1,17 @@
 export function Sidebar() {
   return (
-    <>
+    <div className="flex">
 
-    </>
+  <div className="bg-pink-200 sm:w-60 h-screen w-20 duration-550">
+      SideBar
+
+    </div>
+
+    <div className="bg-green-500 w-full">
+      Content
+    </div>
+    </div>
+
+
   );
 }
