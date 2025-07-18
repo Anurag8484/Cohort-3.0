c090ac9893adfd990b2405c0ff5e3dce4ecb5cfa
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-07-18 12:10:41 +0530

    Make tooltip

--- WebDev/assignments-main/week-13/leetcode-tailwind/src/components/Hero.jsx
+++ WebDev/assignments-main/week-13/leetcode-tailwind/src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { CiLock } from "react-icons/ci";
 import { FaChevronDown, FaStar } from "react-icons/fa";
 import { FaCodeFork, FaFilter } from "react-icons/fa6";
 import { IoIosArrowDown, IoIosCloseCircleOutline } from "react-icons/io";
-import { IoCloseCircle } from "react-icons/io5";
+import { IoCloseCircle, IoRefresh } from "react-icons/io5";
 import { LuDot } from "react-icons/lu";
 import { TiTick } from "react-icons/ti";
 import { VscDebugStart } from "react-icons/vsc";
@@ -11,8 +11,8 @@ export function Hero() {
   return (
     <>
       <section className="grid lg:grid-cols-12  gap-0  my-15 mx-10  w-full">
-        <div className=" bg-[#262626] h-96 min-h-110 mx-5 mb-15 max-w-150 p-5 translate-y-4 min-w-50   lg:col-span-4 rounded-lg shadow-lg overflow-hidden">
-          <div className="border-b-[2px] border-[#333333] pb-5 m-5">
+        <div className=" bg-[#262626]  max-h-138 min-h-146   mb-15 max-w-150 p-0 translate-y-4 min-w-50    lg:col-span-4 rounded-lg shadow-lg overflow-hidden">
+          <div className="border-b-[2px] border-[#333333] pb-5 m-5 ">
             <div className="flex ">
               <span className="bg-amber-50 rounded-[8px] px-1 py-1  text-yellow-500">
                 <FaStar className="size-22 p-5" />
@@ -55,6 +55,68 @@ export function Hero() {
               </div>
             </div>
           </div>
+          <div className="my-1 mx-5">
+            <div className="flex items-center justify-between text-[15px]  ">
+              <span className="text-white font-[500]">Progress</span>
+              <IoRefresh className="text-[#A8A8A8] size-5" />
+            </div>
+            <div className="grid grid-cols-3 gap-2 grid-rows-3 min-h-40 my-1 ">
+              <div className="col-span-2 row-span-3 flex justify-center items-center bg-[#333333] rounded-lg ">
+                <div className="flex size-40   rounded-full relative">
+                  <svg
+                    className="w-full h-full rotate-[-222deg]"
+                    viewBox="0 0 100 100"
+                  >
+                    <circle
+                      cx="50"
+                      cy="50"
+                      r="45"
+                      stroke="#"
+                      stroke-width="3"
+                      fill="none"
+                    />
+                    <circle
+                      cx="50"
+                      cy="50"
+                      r="45"
+                      stroke="#039727"
+                      stroke-width="3"
+                      fill="none"
+                      stroke-linecap="round"
+                      stroke-dasharray="282.6"
+                      stroke-dashoffset="75"
+                    />
+                  </svg>
+                  <div className="absolute inset-0 flex flex-col items-center justify-center text-white text-center">
+                    <div className="font-bold text-3xl">
+                      19<span className="font-[400] text-sm">/19</span>
+                    </div>
+                    <div className="flex  items-center">
+                      <TiTick className="text-[#128729]" />
+                      <span className="text-2sm">Solved</span>
+                    </div>
+                    <div className="flex  items-center translate-y-4   ">
+                      <span className="text-sm font-[400] text-[#A8A8A8]">
+                        0 Atempting
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className=" flex flex-col py-2 font-[600]    items-center  text-sm rounded-lg  bg-[#333333]">
+                <span className="text-[#1CBBBA]">Easy</span>
+                <span className="text-white">11/11</span>
+              </div>
+              <div className=" flex flex-col py-2 font-[600]    items-center  text-sm rounded-lg  bg-[#333333]">
+                <span className="text-[#FFB700]">Med.</span>
+                <span className="text-white">7/7</span>
+              </div>
+              <div className=" flex flex-col py-2 font-[600]    items-center  text-sm rounded-lg  bg-[#333333]">
+                <span className="text-[#DF3937]">Hard</span>
+                <span className="text-white">1/1</span>
+              </div>
+            </div>
+          </div>
         </div>
         <div className=" mx-2   lg:col-span-8 min-h-96 p-4    rounded-lg shadow-lg">
           <div className="flex flex-wrap mb-5 items-center gap-2.5 ">
@@ -69,7 +131,7 @@ export function Hero() {
           </div>
 
           <div className="text-[15px]">
-            <div className=" flex  justify-between  rounded-xl  p-3 bg-[#282828]">
+            <div className=" flex  justify-between  rounded-xl  p-3 bg-[#282828] hover:scale-105 duration-300 cursor-pointer">
               <span className="text-white font-[500] flex items-center gap-3">
                 <TiTick className="text-[#128729]" />
                 14. Longest Common Prefix
@@ -77,70 +139,70 @@ export function Hero() {
               <span className="text-[#1CBBBA] mr-12">Easy</span>
             </div>
             <div>
-              <div className=" flex justify-between  rounded-xl   p-3">
+              <div className=" flex justify-between  rounded-xl hover:scale-105 duration-300 cursor-pointer   p-3">
                 <span className="text-white font-[500] flex items-center gap-3">
                   <TiTick className="text-[#128729]" />
                   217. Contains Duplicate
                 </span>
                 <span className="text-[#1CBBBA] mr-12">Easy</span>
               </div>
-              <div className=" flex justify-between  rounded-xl   p-3  bg-[#282828] ">
+              <div className=" flex justify-between  rounded-xl hover:scale-105 duration-300 cursor-pointer   p-3  bg-[#282828] ">
                 <span className="text-white font-[500] flex items-center gap-3">
                   <TiTick className="text-[#128729]" />
                   125. Valid Palindrome
                 </span>
                 <span className="text-[#1CBBBA] mr-12">Easy</span>
               </div>
-              <div className=" flex justify-between  rounded-xl   p-3 ">
+              <div className=" flex justify-between  rounded-xl hover:scale-105 duration-300 cursor-pointer   p-3 ">
                 <span className="text-white font-[500] flex items-center gap-3">
                   <TiTick className="text-[#128729]" />
                   26. Remove Duplicates from Sorted Array
                 </span>
                 <span className="text-[#1CBBBA] mr-12">Easy</span>
               </div>
-              <div className=" flex justify-between  rounded-xl   p-3 bg-[#282828]">
+              <div className=" flex justify-between  rounded-xl hover:scale-105 duration-300 cursor-pointer   p-3 bg-[#282828]">
                 <span className="text-white font-[500] flex items-center gap-3">
                   <TiTick className="text-[#128729]" />
                   66. Plus One
                 </span>
                 <span className="text-[#1CBBBA] mr-12">Easy</span>
               </div>
-              <div className=" flex justify-between  rounded-xl   p-3 ">
+              <div className=" flex justify-between  rounded-xl hover:scale-105 duration-300 cursor-pointer   p-3 ">
                 <span className="text-white font-[500] flex items-center gap-3">
                   <TiTick className="text-[#128729]" />
                   136. Single Number
                 </span>
                 <span className="text-[#1CBBBA] mr-12">Easy</span>
               </div>
-              <div className=" flex justify-between  rounded-xl   p-3 bg-[#282828]">
+              <div className=" flex justify-between  rounded-xl hover:scale-105 duration-300 cursor-pointer   p-3 bg-[#282828]">
                 <span className="text-white font-[500] flex items-center gap-3">
                   <TiTick className="text-[#128729]" />
                   121. Best Time to Buy and Sell Stock
                 </span>
                 <span className="text-[#1CBBBA] mr-12">Easy</span>
               </div>
-              <div className=" flex justify-between  rounded-xl   p-3 ">
+              <div className=" flex justify-between  rounded-xl hover:scale-105 duration-300 cursor-pointer   p-3 ">
                 <span className="text-white font-[500] flex items-center gap-3">
                   <TiTick className="text-[#128729]" />
                   88. Merge Sorted Array
                 </span>
                 <span className="text-[#1CBBBA] mr-12">Easy</span>
               </div>
-              <div className=" flex justify-between  rounded-xl   p-3 bg-[#282828]">
+              <div className=" flex justify-between  rounded-xl hover:scale-105 duration-300 cursor-pointer   p-3 bg-[#282828]">
                 <span className="text-white font-[500] flex items-center gap-3">
                   <TiTick className="text-[#128729]" />
                   69. Sqrt(x)
                 </span>
                 <span className="text-[#1CBBBA] mr-12">Easy</span>
               </div>
-              <div className=" flex justify-between  rounded-xl   p-3 ">
+              <div className=" flex justify-between  rounded-xl hover:scale-105 duration-300 cursor-pointer   p-3 ">
                 <span className="text-white font-[500] flex items-center gap-3">
                   <TiTick className="text-[#128729]" />
                   206. Reverse Linked List
                 </span>
                 <span className="text-[#1CBBBA] mr-12">Easy</span>
               </div>
-              <div className=" flex justify-between  rounded-xl   p-3 bg-[#282828]">
+              <div className=" flex justify-between  rounded-xl hover:scale-105 duration-300 cursor-pointer   p-3 bg-[#282828]">
                 <span className="text-white font-[500] flex items-center gap-3">
                   <TiTick className="text-[#128729]" />
                   141. Linked List Cycle
