46dae0d41f241449919727aef2159b296cb6d7c0
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-07-18 10:47:13 +0530

    complete hero

--- WebDev/assignments-main/week-13/leetcode-tailwind/src/components/Hero.jsx
+++ WebDev/assignments-main/week-13/leetcode-tailwind/src/components/Hero.jsx
@@ -1,15 +1,17 @@
 import { CiLock } from "react-icons/ci";
 import { FaChevronDown, FaStar } from "react-icons/fa";
-import { FaCodeFork } from "react-icons/fa6";
-import { IoIosArrowDown } from "react-icons/io";
+import { FaCodeFork, FaFilter } from "react-icons/fa6";
+import { IoIosArrowDown, IoIosCloseCircleOutline } from "react-icons/io";
+import { IoCloseCircle } from "react-icons/io5";
 import { LuDot } from "react-icons/lu";
+import { TiTick } from "react-icons/ti";
 import { VscDebugStart } from "react-icons/vsc";
 
 export function Hero() {
   return (
     <>
       <section className="grid lg:grid-cols-12  gap-10  my-15 mx-10  w-full">
-        <div className=" bg-[#262626] h-96 min-h-110 mx-5 max-w-150 p-5 min-w-50   lg:col-span-4 rounded-lg shadow-lg overflow-hidden">
+        <div className=" bg-[#262626] h-96 min-h-110 mx-5 max-w-150 p-5 translate-y-4 min-w-50   lg:col-span-4 rounded-lg shadow-lg overflow-hidden">
           <div className="border-b-[2px] border-[#333333] pb-5 m-5">
             <div className="flex ">
               <span className="bg-amber-50 rounded-[8px] px-1 py-1  text-yellow-500">
@@ -54,8 +56,99 @@ export function Hero() {
             </div>
           </div>
         </div>
-        <div className="bg-yellow-300 mx-5  lg:col-span-8 h-96  rounded-lg shadow-lg">
-          <div>List</div>
+        <div className=" mx-5   lg:col-span-8 min-h-96 p-4    rounded-lg shadow-lg">
+          <div className="flex mb-5 items-center gap-2.5 ">
+            <div className="bg-white flex items-center rounded-2xl px-3 py-1.5 gap-2.5 hover:scale-110 cursor-pointer duration-300">
+              <FaFilter />
+              <span className=" font-[500] text-[13px]">Filter</span>
+            </div>
+            <div className="flex gap-1.5 items-center border-1 text-[13px] border-gray-600 rounded-2xl px-2.5 py-1">
+              <span className="text-white font-[600]">Easy</span>
+              <IoCloseCircle className="text-[#727272] hover:scale-110 cursor-pointer duration-300 hover:text-red-300   size-4" />
+            </div>
+          </div>
+
+          <div>
+            <div className=" flex justify-between  rounded-xl  p-3 bg-[#282828]">
+              <span className="text-white font-[500] flex items-center gap-3">
+                <TiTick className="text-[#128729]" />
+                14. Longest Common Prefix
+              </span>
+              <span className="text-[#1CBBBA] mr-12">Easy</span>
+            </div>
+          </div>
+          <div>
+            <div className=" flex justify-between  rounded-xl   p-3">
+              <span className="text-white font-[500] flex items-center gap-3">
+                <TiTick className="text-[#128729]" />
+                217. Contains Duplicate
+              </span>
+              <span className="text-[#1CBBBA] mr-12">Easy</span>
+            </div>
+            <div className=" flex justify-between  rounded-xl   p-3  bg-[#282828] ">
+              <span className="text-white font-[500] flex items-center gap-3">
+                <TiTick className="text-[#128729]" />
+                125. Valid Palindrome
+              </span>
+              <span className="text-[#1CBBBA] mr-12">Easy</span>
+            </div>
+            <div className=" flex justify-between  rounded-xl   p-3 ">
+              <span className="text-white font-[500] flex items-center gap-3">
+                <TiTick className="text-[#128729]" />
+                14. Longest Common Prefix
+              </span>
+              <span className="text-[#1CBBBA] mr-12">Easy</span>
+            </div>
+            <div className=" flex justify-between  rounded-xl   p-3 bg-[#282828]">
+              <span className="text-white font-[500] flex items-center gap-3">
+                <TiTick className="text-[#128729]" />
+                14. Longest Common Prefix
+              </span>
+              <span className="text-[#1CBBBA] mr-12">Easy</span>
+            </div>
+            <div className=" flex justify-between  rounded-xl   p-3 ">
+              <span className="text-white font-[500] flex items-center gap-3">
+                <TiTick className="text-[#128729]" />
+                14. Longest Common Prefix
+              </span>
+              <span className="text-[#1CBBBA] mr-12">Easy</span>
+            </div>
+            <div className=" flex justify-between  rounded-xl   p-3 bg-[#282828]">
+              <span className="text-white font-[500] flex items-center gap-3">
+                <TiTick className="text-[#128729]" />
+                14. Longest Common Prefix
+              </span>
+              <span className="text-[#1CBBBA] mr-12">Easy</span>
+            </div>
+            <div className=" flex justify-between  rounded-xl   p-3 ">
+              <span className="text-white font-[500] flex items-center gap-3">
+                <TiTick className="text-[#128729]" />
+                14. Longest Common Prefix
+              </span>
+              <span className="text-[#1CBBBA] mr-12">Easy</span>
+            </div>
+            <div className=" flex justify-between  rounded-xl   p-3 bg-[#282828]">
+              <span className="text-white font-[500] flex items-center gap-3">
+                <TiTick className="text-[#128729]" />
+                14. Longest Common Prefix
+              </span>
+              <span className="text-[#1CBBBA] mr-12">Easy</span>
+            </div>
+            <div className=" flex justify-between  rounded-xl   p-3 ">
+              <span className="text-white font-[500] flex items-center gap-3">
+                <TiTick className="text-[#128729]" />
+                14. Longest Common Prefix
+              </span>
+              <span className="text-[#1CBBBA] mr-12">Easy</span>
+            </div>
+            <div className=" flex justify-between  rounded-xl   p-3 bg-[#282828]">
+              <span className="text-white font-[500] flex items-center gap-3">
+                <TiTick className="text-[#128729]" />
+                14. Longest Common Prefix
+              </span>
+              <span className="text-[#1CBBBA] mr-12">Easy</span>
+            </div>
+          </div>
         </div>
       </section>
     </>
