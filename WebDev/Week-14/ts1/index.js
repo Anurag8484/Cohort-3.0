7bb0f4d215369890e77ea6621a9c5ec40132003a
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-07-22 17:07:38 +0530

    init ts file

---
+++ WebDev/Week-14/ts1/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let x = 1;
+x = "harkirat"; // Can't assign this part as it is numb
+console.log(x);
---
+++ WebDev/Week-14/ts1/index.ts
@@ -0,0 +1,5 @@
+let x: number | string = 1;
+x = "harkirat"  // Can't assign this part as it is numb
+
+
+console.log(x);
