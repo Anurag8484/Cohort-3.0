643c3fa76768fb676595206b571513a31eaf4bed
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-08-05 10:51:40 +0530

    make basic ping pong

--- WebDev/Week-16/ws1/src/index.ts
+++ WebDev/Week-16/ws1/src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hi");
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port:8080 });
+
+wss.on('connection', (socket) =>{
+
+    console.log("User Connected 💡");
+    setInterval(()=>{
+        socket.send("Current price of Solana is " + 100 * Math.random());
+    },5*1000)
+
+    socket.on('message', (e)=>{
+        console.log("Yeah, you are bulll");
+        
+    })
+
+})
