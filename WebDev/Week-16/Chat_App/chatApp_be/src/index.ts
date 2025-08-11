be85563f8646b1ff22b2d5c7b93224b839fc1217
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-08-11 16:48:31 +0530

    be almost done

--- WebDev/Week-16/Chat_App/chatApp_be/src/index.ts
+++ WebDev/Week-16/Chat_App/chatApp_be/src/index.ts
@@ -1,12 +1,28 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8080 });
 
 let userCount = 0;
+let allSockets: WebSocket[] = [];
 
 
 wss.on('connection', (socket)=>{
+    allSockets.push(socket);
+
     userCount++;
     console.log("User Connected  #", userCount);
+
+    socket.on("message", (event) =>{
+        console.log("Message Received " + event.toString());
+        for (let i = 0;i<allSockets.length;i++){
+            allSockets[i].send(event.toString() + ": Sent from the server")
+        }
+
+        setTimeout(()=>{
+            socket.send(`You sent message: ${event.toString()}`)
+        },1000)
+
+        
+    })
     
 })
