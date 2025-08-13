1fe46784c2c5d98e2670a455e3deefe92870383c
Anurag8484 <anuragpoonia.ap@gmail.com>
2025-08-13 08:15:13 +0530

    learning transactions

--- WebDev/Week-17/db-basics/src/index.ts
+++ WebDev/Week-17/db-basics/src/index.ts
@@ -11,8 +11,8 @@ const pgClient = new Client({
   port: 5432,
   host: "ep-nameless-rice-adtadpru-pooler.c-2.us-east-1.aws.neon.tech",
   database: "neondb",
-  ssl:{
-    rejectUnauthorized: true
+  ssl: {
+    rejectUnauthorized: false
   }
 });
 pgClient.connect();
@@ -20,8 +20,24 @@ pgClient.connect();
 app.post('/signup', async (req,res)=>{
     const username = req.body.username;
     const password = req.body.password;
+    const email = req.body.email;
+    const city = req.body.city;
+    const country = req.body.country;
+    const street = req.body.street;
+    const pincode = req.body.pincode;
 try {
-    const response = await pgClient.query(`INSERT INTO users (username, password) VALUES ('${username}', '${password}')`)
+
+  const insertQuery = `INSERT INTO users (username, email, password) VALUES ($1,$2,$3) RETURNING id;`
+  const addressInsertQuery = `INSERT INTO addresses (city, country, street, pincode, user_id) VALUES ($1,$2,$3,$4,$5)`
+
+  await pgClient.query('BEGIN;')
+  
+  const response = await pgClient.query(insertQuery,[username, email, password]);
+  const user_id = response.rows[0].id;
+  const addressRespons = await pgClient.query(addressInsertQuery,[city,country,street,pincode, user_id])
+
+  await pgClient.query('COMMIT;')
+
     res.json({
         message:"You have Signed in"
     })
