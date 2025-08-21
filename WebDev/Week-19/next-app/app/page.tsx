6eb20295da26fee05d767686bf520e81d915df9a
anurag8484 <anuragpoonia.ap@gmail.com>
2025-08-21 17:24:28 +0530

    Added style to home

--- WebDev/Week-19/next-app/app/page.tsx
+++ WebDev/Week-19/next-app/app/page.tsx
@@ -1,9 +1,27 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>
-      Hi there
+    <div className=" flex justify-between flex-col items-center">
+      <div className="w-screen h-screen flex p-3  justify-between">
+        <h1 className="text-3xl">Todoist</h1>
+        <div className="flex gap-3 justify-center items-start">
+          <Link
+            className="border-1 p-2 rounded-lg hover:bg-neutral-800"
+            href="/signin"
+          >
+            Sign In
+          </Link>
+          <Link
+            className="border-1 p-2 rounded-lg hover:bg-neutral-800"
+            href="/singup"
+          >
+            {" "}
+            Sign Up
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
