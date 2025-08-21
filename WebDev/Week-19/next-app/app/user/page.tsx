8ca0a6d72eabfc0c8272b0b3cdbd9d9751491ebc
anurag8484 <anuragpoonia.ap@gmail.com>
2025-08-21 16:11:50 +0530

    next js start adv

--- WebDev/Week-19/next-app/app/user/page.tsx
+++ WebDev/Week-19/next-app/app/user/page.tsx
@@ -1,7 +1,24 @@
-export default function User(){
-return(
-    <div>
-        User Page
-    </div>
-)
+import axios, { AxiosResponse } from "axios";
+import { useEffect, useState } from "react"
+
+export default async function User(){
+
+   const response = await  axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+
+   const data = response.data;
+
+
+
+        return (
+          <div>
+            User Page
+            <hr />
+            <br />
+            <div>
+              <span>{data.name}</span>
+              <br />
+               <span>{data.email}</span>
+            </div>
+          </div>
+        );
 }
