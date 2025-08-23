01b398a0ff6cc811c743de97dec9ff94faf8cc56
anurag8484 <anuragpoonia.ap@gmail.com>
2025-08-23 17:49:25 +0530

    init catch all segment

--- WebDev/Week-20/nextjs-adv/app/blog/[postId]/page.tsx
+++ WebDev/Week-20/nextjs-adv/app/blog/[postId]/page.tsx
@@ -1,10 +1,26 @@
-export default function blogs({params}:{params:{
-    postId: string
-}}){
-    const postId = params.postId;
+import axios from "axios";
+
+export default async function blogs({ params }: { params: { postId: string } }) {
+    const postId = (await params).postId;
+    let res
+    let data
+    try {
+         res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+         data = res.data;
+        
+    } catch (error) {
+        console.error(error)
+    }
+
+
+
     return(
         <div>
-            Blog Page
+            Blog Page {postId}
+            <br />
+            <div>
+                {data.title}
+            </div>
         </div>
     )
 }
