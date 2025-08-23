e9918052aa7485d41a31bdcd36be241faa622113
anurag8484 <anuragpoonia.ap@gmail.com>
2025-08-23 17:25:46 +0530

    added route group

---
+++ WebDev/Week-20/nextjs-adv/app/(auth)/signup/page.tsx
@@ -0,0 +1,13 @@
+export default function signup() {
+  return (
+    <div>
+      Signup
+      <br />
+      <input className="outline-1" type="text" name="" id="" />
+      <br />
+      <input className="outline-1" type="text" name="" id="" />
+      <br />
+      <button>Submit</button>
+    </div>
+  );
+}
