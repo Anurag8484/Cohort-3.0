747f517fc5724e7bdbb66f9a2961eca0ea980fa4
anurag8484 <anuragpoonia.ap@gmail.com>
2025-08-26 10:48:58 +0530

    basic tr done

--- WebDev/Week-21/chat-app/apps/http-server/src/index.ts
+++ WebDev/Week-21/chat-app/apps/http-server/src/index.ts
@@ -2,3 +2,16 @@
 import express from "express";
 
 const app = express();
+
+app.get("/signup",(req,res)=>{
+    res.send("Hello World")
+})
+app.get("/signin",(req,res)=>{
+    res.send("Hello World")
+})
+app.get("/chat",(req,res)=>{
+    res.send("Hello World")
+})
+
+
+app.listen(3001);
